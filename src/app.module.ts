06c010042f599348266b5452e0f04da8546cb68b
breno-aredes <breno_mg10@hotmail.com>
2023-06-29 17:22:17 -0300

    feat: create resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PublicationModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
